874048157b9a0453b6c6bb03876cc4d9e0475d8d
Mazher <mazheriftikhar1@gmail.com>
2025-04-24 23:41:14 +0500

    UI-FIXES

--- crypto-website/src/components/Navbar/Footer/Footer.jsx
+++ crypto-website/src/components/Navbar/Footer/Footer.jsx
@@ -1,12 +1,26 @@
-import React from 'react'
-import './Footer.css'
+import React from 'react';
+import './Footer.css';
+import { FaGithub, FaLinkedin, FaTwitter } from 'react-icons/fa';
 
 const Footer = () => {
   return (
-    <div className='footer'>
-      <p>Copyright @ 2024, Cryptico - All right Reserved</p>
-    </div>
-  )
-}
+    <footer className="footer">
+      <div className="footer-content">
+        <p>© 2024 Cryptico. All rights reserved.</p>
+        <div className="footer-icons">
+          <a href="https://github.com/" target="_blank" rel="noreferrer">
+            <FaGithub />
+          </a>
+          <a href="https://linkedin.com/" target="_blank" rel="noreferrer">
+            <FaLinkedin />
+          </a>
+          <a href="https://twitter.com/" target="_blank" rel="noreferrer">
+            <FaTwitter />
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
